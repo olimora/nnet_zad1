97c6a2bd1462c6a547745f26751f28c028cd4a1a
oliver3541 <o.moravcik@gmail.com>
2017-10-28 16:54:37 +0200

    multiple layers and activ. func. as input

--- classifier.py
+++ classifier.py
@@ -6,43 +6,20 @@ from util import *
 
 class MLPClassifier(MLP):
 
-    def __init__(self, dim_in, dim_hid, n_classes):
-        self.n_classes = n_classes
-        super().__init__(dim_in, dim_hid, dim_out=n_classes)
+    def __init__(self, dims, functions):
+        self.n_classes = dims[-1]
+        super().__init__(dims, functions)
 
-    
-    ## functions
-    # https://en.wikipedia.org/wiki/Activation_function
-    # sigmoid == logistic == soft step
 
     def cost(self, targets, outputs): # new
         return np.sum((targets - outputs)**2, axis=0)
 
-    def f_hid(self, x): # override
-        # FIXME sigmoid or tanh
-        return 1 / (1 + (np.e ** (-x)))  # sigmoid
-        # return (2 / (1 + (np.e ** (-2*x)))) - 1  # TanH
-
-    def df_hid(self, x): # override
-        # FIXME corresponding derivation
-        return self.f_hid(x) * (1 - self.f_hid(x))  # sigmoid
-        # return 1 - (self.f_hid(x)**2) # TanH
-
-    def f_out(self, x): # override
-        # FIXME sigmoid or softmax # try binary step
-        return 1 / (1 + (np.e ** (-x)))  # sigmoid 8.00%, RE = 0.28998;6.00%, RE = 0.26435
-        # return np.exp(x) / np.sum(np.exp(x)) # softmax # ,axis = 0 #subtract max? https://stackoverflow.com/questions/34968722/softmax-function-python
-
-    def df_out(self, x): # override
-        # FIXME corresponding derivation
-        return self.f_hid(x) * (1 - self.f_hid(x))  # sigmoid
-        # softmax https://stackoverflow.com/questions/36279904/softmax-derivative-in-numpy-approaches-0-implementation
-
-
     ## prediction pass
 
     def predict(self, inputs):
-        outputs, *_ = self.forward(inputs)  # if self.forward() can take a whole batch
+        # outputs, *_ = self.forward(inputs)  # if self.forward() can take a whole batch
+        _, outputs = self.forward(inputs)
+        outputs = outputs[-1]
         # outputs = np.stack([self.forward(x)[0] for x in inputs.T]) # otherwise
         return onehot_decode(outputs)
 
@@ -50,7 +27,9 @@ class MLPClassifier(MLP):
     ## testing pass
 
     def test(self, inputs, labels):
-        outputs, *_ = self.forward(inputs) # FIXME
+        # outputs, *_ = self.forward(inputs) # FIXME
+        _, outputs = self.forward(inputs)
+        outputs = outputs[-1]
         targets = onehot_encode(labels, self.n_classes) # FIXME
         predicted = onehot_decode(outputs) # FIXME
         CE = np.sum(labels != predicted) / inputs.shape[1] # FIXME
@@ -78,13 +57,13 @@ class MLPClassifier(MLP):
                 x = inputs[:, i] # FIXME
                 d = targets[:, i] # FIXME
 
-                y, dW_hid, dW_out = self.backward(x, d)
+                y, dWs = self.backward(x, d)
 
                 CE += labels[i] != onehot_decode(y)
                 RE += self.cost(d,y)
 
-                self.W_hid += alpha*dW_hid.T # FIXME
-                self.W_out += alpha*dW_out.T # FIXME
+                for i in range(self.nlayers-1):
+                    self.weights[i] += alpha*dWs[i].T
 
             CE /= count
             RE /= count
@@ -92,9 +71,12 @@ class MLPClassifier(MLP):
             CEs.append(CE)
             REs.append(RE)
 
-            if (ep+1) % 10 == 0:
-                print('Model {:1d}: Ep {:3d}/{}: '.format(model_num, ep+1, eps), end='')
-                print('CE = {:6.2%}, RE = {:.5f}'.format(CE, RE))
+            # if (ep+1) % 10 == 0:
+            #     print('Model {:1d}: Ep {:3d}/{}: '.format(model_num, ep+1, eps), end='')
+            #     print('CE = {:6.2%}, RE = {:.5f}'.format(CE, RE))
+
+            print('Model {:1d}: Ep {:3d}/{}: '.format(model_num, ep + 1, eps), end='')
+            print('CE = {:6.2%}, RE = {:.5f}'.format(CE, RE))
 
             if trace and ((ep+1) % trace_interval == 0):
                 clear()
---
+++ func.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+
+## general activations
+
+def relu(X):
+    return np.where(X>0, X, 0)
+
+def drelu(X):
+    return np.where(X>0, 1, 0)
+
+
+def logsig(X):
+    return 1 / (1 + np.exp(-X))
+    # return 1 / (1 + (np.e ** (-X)))
+
+def dlogsig(X):
+    return logsig(X) * (1 - logsig(X))
+
+
+def tanh(X):
+    return np.tanh(X)
+
+def dtanh(X):
+    return 1 / np.cosh(X)**2
+
+
+## output-only activations
+
+def linear(X):
+    return X
+
+def dlinear(X):
+    return np.ones(X.shape)
+
+
+def softmax(X):
+    # X = np.atleast_2d(X)
+    # return (np.exp(X) / np.sum(np.exp(X), axis=1))
+    return (np.exp(X) / np.sum(np.exp(X)))
+
+def dsoftmax(X):
+    # X = np.atleast_2d(X)
+    # soft = np.exp(X) / np.sum(np.exp(X), axis=1)
+    # out = np.zeros(X.shape)
+    # out[range(X.shape[0]), np.argmax(X, axis=1)] = soft[range(X.shape[0]), np.argmax(X, axis=1)]
+    # return out
+    out = np.zeros(X.shape)
+    out[np.argmax(X)] = np.exp(X[np.argmax(X)]) / np.sum(np.exp(X))
+    return out
+
+
+## cost functions
+
+def MSE(D, Y):
+    return np.sum((D - Y)**2, axis=0)
+
+# TODO dMSE
+
+
+def CE(D, Y):
+    return -np.sum(np.log(Y) * D, axis=0)
+
+# TODO dCE
