468c1c44cec189b93c1fc77babe771f47c027815
oliver3541 <o.moravcik@gmail.com>
2017-10-30 11:13:50 +0100

    misspell and model parameters in parallel cross validation

--- main.py
+++ main.py
@@ -5,35 +5,40 @@ from classifier import *
 import multiprocessing as mp
 from itertools import repeat
 
-def parallel_cross_validation(split_id, passed_data):
+def parallel_cross_validation(split_ID, passed_data):
     #passed data from common main
     split = passed_data[0]
     train_inputs = passed_data[1]
     train_labels = passed_data[2]
     model_ID = passed_data[3]
+    parameters = passed_data[4]
 
     #indexes of validation entries are in dedicated split
-    valid_ind = split[split_id]
+    valid_ind = split[split_ID]
     #indexes of estimation entries are all the others splits combined
     #so concantenate indexes from list 'split' on indexes 0:10 without split_id
-    estim_ind = np.concatenate(split[np.delete(np.arange(10), split_id, axis=0)])
+    estim_ind = np.concatenate(split[np.delete(np.arange(10), split_ID, axis=0)])
     estim_inputs = train_inputs[:, estim_ind]
     estim_labels = train_labels[estim_ind]
     valid_inputs = train_inputs[:, valid_ind]
     valid_labels = train_labels[valid_ind]
 
     ## train & validate
-    model = MLPClassifier([train_inputs.shape[0], 20, 6, np.max(train_labels) + 1],
-                          ['tanh', 'sig', 'lin'], ['uniform', [0, 1]],
-                          model_ID=model_ID, split_id=split_id)
+    model = MLPClassifier(parameters[1], parameters[2], parameters[3],
+                          model_ID=model_ID, split_ID=split_ID)
     trainCEs, trainREs = model.train(estim_inputs, estim_labels, valid_inputs, valid_labels,
-                                     alpha=0.05, momentum=0.1,
-                                     min_accuracy=97, max_epoch=500, min_delay_expectancy=50,
-                                     q_size=30, raised_err_threashold=0.66, acc_err_threshold=0.001,
+                                     alpha=parameters[4],
+                                     momentum=parameters[5],
+                                     min_accuracy=parameters[6],
+                                     max_epoch=parameters[7],
+                                     min_delay_expectancy=parameters[8],
+                                     q_size=parameters[9],
+                                     raised_err_threashold=parameters[10],
+                                     acc_err_threshold=parameters[11],
                                      trace=False, trace_interval=10)
     validCE, validRE = model.test(valid_inputs, valid_labels)
 
-    return np.array([split_id, validCE, validRE])
+    return np.array([split_ID, validCE, validRE])
 
 if __name__ == '__main__':
 
@@ -53,13 +58,13 @@ if __name__ == '__main__':
         ['abs', # data normalization type
         [2, 20, 6, 3], ['tanh', 'sig', 'lin'], ['uniform', [0, 1]], # layers, functions, distribution and scale
         0.05, 0.1,          # aplha, momentum
-        97, 500, 50,        # min_accuracy, max_epoch, min_delay_expectancy
+        97, 5, 50,        # min_accuracy, max_epoch, min_delay_expectancy
         30, 0.66, 0.001])   # q_size, raised_err_threashold, acc_err_threshold
     hyperparameters.append(
         ['std', # data normalization type
         [2, 12, 6, 3], ['sig', 'sig', 'sig'], ['normal', [-1, 1]], # layers, functions, distribution and scale
         0.05, 0.05,         # aplha, momentum
-        97, 500, 50,        # min_accuracy, max_epoch, min_delay_expectancy
+        97, 5, 50,        # min_accuracy, max_epoch, min_delay_expectancy
         20, 0.66, 0.001])   # q_size, raised_err_threashold, acc_err_threshold
 
 
@@ -81,9 +86,10 @@ if __name__ == '__main__':
 
         # parallel cross validation
         pool = mp.Pool(processes=4)  # removing processes argument makes the code run on all available cores
-        results = np.array(
-            pool.starmap(parallel_cross_validation, zip(np.arange(10), repeat([split, train_inputs, train_labels, i]))))
+        results = np.array(pool.starmap(parallel_cross_validation,
+            zip(np.arange(10), repeat([split, train_inputs, train_labels, i, hyperparameters[i]]))))
         print(results)
+        print(results.shape)
 
         # mean from results
 
